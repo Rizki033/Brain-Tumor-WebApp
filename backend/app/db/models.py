74f41d23e102ad1f5a81d4f8bdf4a0b6233095c9
Mohamed EL IDRISSI <mohamedelidrissi443@gmail.com>
2026-01-12 23:13:31 +0100

    Add the Model 'Patient' to the file models.py

--- backend/app/db/models.py
+++ backend/app/db/models.py
@@ -4,8 +4,14 @@ from datetime import datetime
 from fastapi import Depends, FastAPI, HTTPException, Query
 from sqlmodel import Field, Session, SQLModel, create_engine, select
 
+from enum import StrEnum
 
-# ---------------------- Model Doctor ---------------------
+from sqlmodel import SQLModel, Field
+from sqlalchemy import Column                # ← this was missing
+from sqlalchemy.types import Enum
+
+
+# ---------------------- Doctor Model ---------------------
 class Doctor(SQLModel, table=True):
     id: int | None = Field(default=None, primary_key=True)
     name: str = Field(index=True)
@@ -29,3 +35,23 @@ class Doctor(SQLModel, table=True):
         # logic of patient' account creation
         return
     
+
+# ---------------------- Patient Model ---------------------
+class Patient(SQLModel, table=True):
+    id: int | None = Field(default=None, primary_key=True)
+    first_name: str = Field(index=True)
+    last_name: str = Field(index=True) 
+    gender: Optional[str] = Field(
+        default=None,
+        sa_column=Column(Enum(
+            "male", "female", name="gender_enum"
+        ))
+    )
+    age: int | None = Field(default=None, index=True)
+    
+    
+    created_at: datetime = Field(
+        default_factory=datetime.utcnow,
+        nullable=False,
+        sa_column_kwargs={"server_default": "CURRENT_TIMESTAMP"}  # SQLite loves this!
+    )
